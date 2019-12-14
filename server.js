b42499c23f2ad0fd8ac0efbcbfd932de569ea1ed
petterigit <33552590+petterigit@users.noreply.github.com>
2019-12-14 17:14:05 +0200

    Update server.js

--- server.js
+++ server.js
@@ -6,10 +6,8 @@ const logger = require('morgan');
 const Data = require('./data');
 var http = require('http');
 
-const 	API_PORT = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 3000,
+const 	API_PORT = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
 		ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
-
-var API_PORT = normalizePort(API_PORT || '3000');
 
 const app = express();
 
